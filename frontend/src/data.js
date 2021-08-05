ca2c8fb8a63ef95f0d29ccc73f44b9ebfdf465a5
unknown <nqtuan.19it@vku.udn.vn>
2021-08-05 12:27:41 +0700

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name: 'ten san pham1',
+            category: 'loai san pham',
+            image: '/images/p1.jpg',
+            price: 120000,
+            brand: 'hang san pham',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Mo ta san pham'
+        },
+        {
+            _id:'2',
+            name: 'ten san pham2',
+            category: 'loai san pham',
+            image: '/images/p1.jpg',
+            price: 120000,
+            brand: 'hang san pham',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Mo ta san pham'
+        },
+        {
+            _id:'3',
+            name: 'ten san pham3',
+            category: 'loai san pham',
+            image: '/images/p1.jpg',
+            price: 120000,
+            brand: 'hang san pham',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Mo ta san pham'
+        },
+        {
+            _id:'4',
+            name: 'ten san pham4',
+            category: 'loai san pham',
+            image: '/images/p1.jpg',
+            price: 120000,
+            brand: 'hang san pham',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Mo ta san pham'
+        },
+        {
+            _id:'5',
+            name: 'ten san pham5',
+            category: 'loai san pham',
+            image: '/images/p1.jpg',
+            price: 120000,
+            brand: 'hang san pham',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Mo ta san pham'
+        },
+        {
+            _id:'6',
+            name: 'ten san pham6',
+            category: 'loai san pham',
+            image: '/images/p1.jpg',
+            price: 120000,
+            brand: 'hang san pham',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Mo ta san pham'
+        },   
+    ],
+};
+export default data;
